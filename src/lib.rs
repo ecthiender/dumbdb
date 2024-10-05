286e92510f38d7c4804954e806bba023f189c2f9
Anon Ray <rayanon004@gmail.com>
2024-10-05 21:35:20 +0530

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@ pub struct PutItemCommand {
 
 type Record = HashMap<String, PrimitiveValue>;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(untagged)]
 pub enum PrimitiveValue {
     Integer(u64),
@@ -176,8 +176,7 @@ pub fn get_item(command: GetItemCommand, catalog: &Catalog) -> anyhow::Result<Re
 
 fn parse_record(columns: &Vec<ColumnDefinition>, item: &str) -> anyhow::Result<Record> {
     let mut result = HashMap::new();
-    let parts = item.split(",");
-    for (idx, part) in parts.enumerate() {
+    for (idx, part) in item.split(",").enumerate() {
         let col_name = columns[idx].name.clone();
         let val = PrimitiveValue::from_string(part.to_string());
         result.insert(col_name, val);
@@ -295,6 +294,7 @@ fn create_table_on_disk(table: &CreateTableCommand, catalog: &Catalog) -> anyhow
 fn write_to_file(file: &mut File, data: String) -> anyhow::Result<()> {
     writeln!(file, "{}", data)
         .with_context(|| "FATAL: Internal Error: Failed writing data to file")?;
+    let _ = file.flush();
     Ok(())
 }
 
@@ -325,4 +325,113 @@ fn write_json_file<T: Serialize>(file_path: &PathBuf, item: &T) -> anyhow::Resul
     // Serialize the item struct to JSON and write it to the file.
     serde_json::to_writer(writer, &item)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::Rng;
+    use serde_json::json;
+
+    use super::*;
+
+    const DB_PATH: &str = "./data/test/dumbdb";
+
+    #[test]
+    fn test_create_table() -> anyhow::Result<()> {
+        let catalog = setup("create_table")?;
+        assert!(catalog.get_table("authors").is_some());
+        Ok(())
+    }
+
+    #[test]
+    fn test_write_data() -> anyhow::Result<()> {
+        let mut catalog = setup("write_data")?;
+        for i in 0..10 {
+            let author_item = create_put_item(i)?;
+            put_item(author_item, &mut catalog)?;
+        }
+        let table_rel_path = PathBuf::from(format!("authors.tbl"));
+        let table_path = catalog.directory_path.join(table_rel_path);
+        let contents = fs::read_to_string(table_path)?;
+        let last_line = contents
+            .lines()
+            .filter(|x| !x.is_empty())
+            .last()
+            .with_context(|| "There should be 10 rows written")?;
+        assert!(last_line.to_string().starts_with("9,"));
+        Ok(())
+    }
+
+    #[test]
+    fn test_read_data() -> anyhow::Result<()> {
+        let mut catalog = setup("read_data")?;
+        for i in 0..10 {
+            let author_item = create_put_item(i)?;
+            put_item(author_item, &mut catalog)?;
+        }
+        for i in 5..8 {
+            let cmd = create_get_item(i)?;
+            let record = get_item(cmd, &catalog)?;
+            assert_eq!(
+                &PrimitiveValue::Integer(i as u64),
+                record.get("id").unwrap()
+            );
+        }
+        Ok(())
+    }
+
+    fn setup(test_name: &str) -> anyhow::Result<Catalog> {
+        let authors_table = json!({
+            "name": "authors",
+            "columns": [
+                {
+                    "name": "id",
+                    "type": "Integer",
+                },
+                {
+                    "name": "name",
+                    "type": "Text",
+                }
+            ],
+            "primary_key": "id"
+        });
+
+        let root_test_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        // if test dir already exists; remove it.
+        let dir_path = root_test_dir.join(format!("{}_{}", DB_PATH, test_name));
+        if dir_path.exists() {
+            let _ = fs::remove_dir_all(&dir_path);
+        }
+        fs::create_dir_all(&dir_path)?;
+        let mut catalog = Catalog::new(dir_path)?;
+        create_table(serde_json::from_value(authors_table)?, &mut catalog)?;
+        Ok(catalog)
+    }
+
+    fn create_get_item(id: u64) -> anyhow::Result<GetItemCommand> {
+        Ok(serde_json::from_value(json!({
+            "table_name": "authors",
+            "key": id.to_string(),
+        }))?)
+    }
+
+    fn create_put_item(id: u64) -> anyhow::Result<PutItemCommand> {
+        const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        const STRING_LEN: usize = 10;
+        let mut rng = rand::thread_rng();
+        let rand_string: String = (0..STRING_LEN)
+            .map(|_| {
+                let idx = rng.gen_range(0..CHARSET.len());
+                CHARSET[idx] as char
+            })
+            .collect();
+
+        Ok(serde_json::from_value(json!({
+            "table_name": "authors",
+            "item": {
+                "id": id,
+                "name": rand_string,
+            }
+        }))?)
+    }
 }
